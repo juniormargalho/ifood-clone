144a321bac19969b83dd5608645b29f0061a8301
juniormargalho <juniormargalho@gmail.com>
2020-06-12 15:38:34 -0300

    toolbar

---
+++ app/src/main/java/com/juniormargalho/ifood/activity/ConfiguracoesEmpresaActivity.java
@@ -0,0 +1,16 @@
+package com.juniormargalho.ifood.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.juniormargalho.ifood.R;
+
+public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes_empresa);
+    }
+}
